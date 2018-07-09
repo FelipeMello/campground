2052769f708100f76e5915d0c2fc2b63c92e4d3d
felipeMello <felipe.silva.mello@gmail.com>
2018-07-09 19:02:03 +0100

    post a comment in campground

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const   express     = require("express"),
         bodyParser  = require("body-parser"),
         mongoose    = require("mongoose"),
         Campground  = require("./models/campground"),
+        Comment     = require("./models/comment"),
         seedDB      = require("./seeds");
     
 
@@ -90,6 +91,30 @@ app.get("/campgrounds/:id", function(req, res){
     })
     
 
+ });
+
+ app.post("/campgrounds/:id/comments", function(req, res){
+     //Lookup campground by ID
+     Campground.findById(req.params.id, function(err, campground){
+         if(err){
+             console.log(err);
+             res.redirect("/campgrounds");
+         }else{
+            //Create a new comment
+            //connect new comment to campground
+            //redirect to campground show page.
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/'+campground._id);
+                }
+            })
+         }
+     })
+     
  })
 
 const port = 3000;
